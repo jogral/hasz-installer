777673201838e388edfef9d969bccb10a8addf49
makiten <makiten@angk.org>
2016-11-03 00:21:01 -0500

    Some additional fixes and tweaks to get Hadoop running and replacing config files.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# dswb-installer
-A Bash script to install the tools (plus Hadoop and Emacs 25) needed for https://datascientistworkbench.com
+# Data Science Workbench Installer (dswb-installer)
+I had to create an Ubuntu image twice of the tools I wanted to use for the [Data Science Workbench](https://datascienceworkbench.com/), because I couldn't export the original image.
+Since setting up Hadoop, Spark, and Zeppelin are sort of a chore, I wrote a quick and dirty script to install. Right now, this has been tested on **Ubuntu 16.04 LTS**. I'll test other distros
+when I'm not lazy.
+
+Instructions
+------------------------
+Well, this one is easy. Simply run:
+
+	git clone https://github.com/makiten/dswb-installer.git
+	cd dswb-installer
+	./setup.sh
+
+And follow the prompts that will come up, and watch it install.
+
+
+License
+-------------------------
+This repo is open source under the MIT License.
+
+TODO
+-------------------------
+* Thorough testing
+* Cleanup on failure
--- setup.sh
+++ setup.sh
@@ -1,33 +1,174 @@
 #!/bin/bash
+######################################
+## VARS                             ##
+######################################
+CWD=$(pwd)
+HADOOP_MAJOR_VER="2.7"
+HADOOP_MINOR_VER="3"
+HADOOP_VER="${HADOOP_MAJOR_VER}.${HADOOP_MINOR_VER}"
+RSTUDIO_VER="1.0.44"
+SPARK_VER="2.0.1"
+ANACONDA3_VER="4.2.0"
+ZEPPELIN_VER="0.6.2"
 
+RSTUDIO_FILENAME="rstudio-${RSTUDIO_VER}-amd64.deb"
+HADOOP_FILENAME="hadoop-${HADOOP_VER}.tar.gz"
+SPARK_FILENAME="spark-${SPARK_VER}-bin-hadoop${HADOOP_MAJOR_VER}.tgz"
+ANACONDA3_FILENAME="Anaconda3-${ANACONDA3_VER}-Linux-x86_64.sh"
+ZEPPELIN_FILENAME="zeppelin-${ZEPPELIN_VER}-bin-all.tgz"
+
+RSTUDIO_DOWNLOAD_URL="https://download1.rstudio.org/${RSTUDIO_FILENAME}"
+HADOOP_DOWNLOAD_URL="http://mirrors.ibiblio.org/apache/hadoop/common/hadoop-${HADOOP_VER}/${HADOOP_FILENAME}"
+SPARK_DOWNLOAD_URL="http://d3kbcqa49mib13.cloudfront.net/${SPARK_FILENAME}"
+ANACONDA3_DOWNLOAD_URL="https://repo.continuum.io/archive/${ANACONDA3_FILENAME}"
+ZEPPELIN_DOWNLOAD_URL="http://mirrors.ibiblio.org/apache/zeppelin/zeppelin-${ZEPPELIN_VER}/${ZEPPELIN_FILENAME}"
+#-------------------------------------
+
+######################################
+## HELPER ACTIONS                   ##
+######################################
 function set_hadoop_envvars() {
 echo """
-export JAVA_HOME=$(readlink -f /usr/bin/java | sed \"s:bin/java::\")
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed 's:bin/java::')
 
 # Hadoop Environmental Variables
 export HADOOP_HOME=\"/usr/local/share/hadoop\"
-export HADOOP_MAPRED_HOME=\"$HADOOP_HOME\"
-export HADOOP_COMMON_HOME=\"$HADOOP_HOME\"
-export HADOOP_HDFS_HOME=\"$HADOOP_HOME\"
-export YARN_HOME=\"$HADOOP_HOME\"
-export HADOOP_CONF_DIR=\"$HADOOP_HOME/etc/hadoop\"
-export YARN_CONF_DIR=\"$HADOOP_HOME/etc/hadoop\"
-export HADOOP_COMMON_LIB_NATIVE_DIR=\"$HADOOP_HOME/lib/native\"
+export HADOOP_MAPRED_HOME=\"\$HADOOP_HOME\"
+export HADOOP_COMMON_HOME=\"\$HADOOP_HOME\"
+export HADOOP_HDFS_HOME=\"\$HADOOP_HOME\"
+export YARN_HOME=\"\$HADOOP_HOME\"
+export HADOOP_CONF_DIR=\"\$HADOOP_HOME/etc/hadoop\"
+export YARN_CONF_DIR=\"\$HADOOP_HOME/etc/hadoop\"
+export HADOOP_COMMON_LIB_NATIVE_DIR=\"\$HADOOP_HOME/lib/native\"
 export HADOOP_OPTS=\"-Djava.library.path=$HADOOP_HOME/lib\"
-export PATH=\"$HADOOP_HOME/bin:$PATH\"
-export PATH=\"$HADOOP_HOME/sbin:$PATH\"
+export PATH=\"\$HADOOP_HOME/bin:\$PATH\"
+export PATH=\"\$HADOOP_HOME/sbin:\$PATH\"
 """
 }
+# 15 lines
 function set_spark_envvars() {
 echo """
 export SPARK_HOME=\"/usr/local/share/spark\"
-export PATH=\"$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH\"
+export PATH=\"\$SPARK_HOME/bin:\$SPARK_HOME/sbin:\$PATH\"
 """
 }
+# 17 lines
+#-------------------------------------
+######################################
+## INSTALL ACTIONS                  ##
+######################################
+##### Installing RStudio #############
+function install_rstudio() {
+if [ ! -f $HOME/Downloads/$RSTUDIO_FILENAME ]; then
+	curl -L $RSTUDIO_DOWNLOAD_URL -o $HOME/Downloads/$RSTUDIO_FILENAME
+elif ! hash rstudio 2>/dev/null; then
+	sudo dpkg -i $HOME/Downloads/$RSTUDIO_FILENAME
+fi
+}
+#-------------------------------------
+##### Installing Hadoop ##############
+function install_hadoop() {
+sudo addgroup hadoop
+sudo adduser --ingroup hadoop hduser
+# Generate SSH key for Hadoop
+sudo su hduser -c "ssh localhost 'exit'"
+sudo su hduser -c "ssh-keygen -t rsa -b 4096 -P \"\"; cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys" 
+
+# Set Hadoop env variables
+set_hadoop_envvars >> $HOME/.bashrc && source $HOME/.bashrc
+set_hadoop_envvars | sudo tee --append /home/hduser/.bashrc
+sudo su hduser -c "source /home/hduser/.bashrc; echo JAVA_HOME=${JAVA_HOME}"
+
+if [ ! -f $HOME/Downloads/$HADOOP_FILENAME ]; then
+	curl -L $HADOOP_DOWNLOAD_URL -o $HOME/Downloads/$HADOOP_FILENAME
+fi
+
+tar xzvf $HADOOP_FILENAME
+sudo mv hadoop-2.7.3 /usr/local/share
+HADOOP_PATH="/usr/local/share/hadoop-2.7.3"
+cd /usr/local/share && sudo ln -s $HADOOP_PATH hadoop && cd $HOME/Downloads
+
+if [ ! -d $CWD/scripts/ ]; then
+	echo The scripts directory is missing.
+	exit 1
+else
+	$CWD/scripts/configure-hadoop.sh $HADOOP_PATH
+fi
+
+sudo chown -R hduser:hadoop $HADOOP_PATH
+sudo su hduser -c "${HADOOP_PATH}/bin/hdfs namenode -format"
+sudo su hduser -c "${HADOOP_PATH}/sbin/start-dfs.sh; jps; ${HADOOP_PATH}/sbin/start-yarn.sh"
+}
+#-------------------------------------
+##### Install Spark ##################
+function install_spark() {
+SPARK_PATH="/usr/local/share/spark-${SPARK_VER}-bin-hadoop${HADOOP_MAJOR_VER}"
+set_spark_envvars >> $HOME/.bashrc ; source $HOME/.bashrc
+sudo addgroup analysts
+sudo usermod -a -G analysts $(whoami)
+if [ ! -f $HOME/Downloads/$SPARK_FILENAME ]; then
+	curl -L $SPARK_DOWNLOAD_URL -o $HOME/Downloads/$SPARK_FILENAME
+fi
+tar xzvf $HOME/Downloads/$SPARK_FILENAME
+sudo mv spark-$SPARK_VER-bin-hadoop$HADOOP_MAJOR_VER /usr/local/share; cd /usr/local/share && sudo ln -s $SPARK_PATH spark && cd $HOME/Downloads; sudo chown -R $(whoami):analysts $SPARK_PATH
+sudo cp $SPARK_PATH/conf/spark-env.sh.template $SPARK_PATH/conf/spark-env.sh
+echo """
+JAVA_HOME=$(readlink -f /usr/bin/java | sed 's:bin/java::')
+SPARK_MASTER_IP=10.0.2.15
+SPARK_WORKER_MEMORY=3g
+""" | sudo tee --append $SPARK_PATH/conf/spark-env.sh
+}
+#-------------------------------------
+##### Installing Anaconda ############
+function install_anaconda() {
+if [ ! -f $HOME/Downloads/$ANACONDA3_FILENAME ]; then
+	curl -L $ANACONDA3_DOWNLOAD_URL -o $HOME/Downloads/$ANACONDA3_FILENAME
+fi
+bash $ANACONDA3_FILENAME
+source $HOME/.bashrc
+}
+#-------------------------------------
+##### Installing Apache Zeppelin #####
+function install_zeppelin() {
+sudo adduser --system --ingroup analysts zeppelin
+if [ ! -f $HOME/Downloads/$ZEPPELIN_FILENAME ]; then
+	curl -L $ZEPPELIN_DOWNLOAD_URL -o $HOME/Downloads/$ZEPPELIN_FILENAME
+fi
+tar xzvf $HOME/Downloads/$ZEPPELIN_FILENAME
+sudo mv zeppelin-$ZEPPELIN_VER-bin-all /usr/local && sudo ln -s zeppelin-$ZEPPELIN_VER-bin-all zeppelin
+sudo chown -R zeppelin:analysts /usr/local/share/zeppelin
+echo """
+description \"zeppelin\"
+
+start on (local-filesystems and net-device-up IFACE!=lo)
+stop on shutdown
+
+# Respawn the process on unexpected termination
+respawn
+
+# respawn the job up to 7 times within a 5 second period.
+# If the job exceeds these values, it will be stopped and marked as failed.
+respawn limit 7 5
+
+# zeppelin was installed in /usr/share/zeppelin in this example
+chdir /usr/local/share/zeppelin
+exec bin/zeppelin-daemon.sh upstart
+""" | sudo tee /etc/init/zeppelin.conf
+sudo service zeppelin start
+}
+#-------------------------------------
+######################################
+## SCRIPT                           ##
+######################################
+
+#if [ "$EUID" -ne 0 ]; then
+#	echo You need sudo privileges to run this.
+#	exit 1
+#else
 
 cd $HOME/Downloads
 
-##### Get all the packages for everything #####
+##### Get all the packages ###########
 sudo add-apt-repository -y ppa:ubuntu-elisp
 sudo apt-get -y update
 sudo apt-get -y install emacs-snapshot\
@@ -45,167 +186,21 @@ sudo apt-get -y install emacs-snapshot\
 			r-base\
 			r-base-dev
 
-##### Checking stuff #####
+##### Checking stuff #################
 java -version; scala -version
 
-##### Installing RStudio #####
-wget https://download1.rstudio.org/rstudio-1.0.44-amd64.deb
-sudo dpkg -i rstudio-1.0.44-amd64.deb
+##### Install ########################
+install_rstudio
+install_hadoop
+install_spark
+install_anaconda
+install_zeppelin
 
-##### Installing Hadoop #####
-sudo addgroup hadoop
-sudo adduser --ingroup hadoop hduser
-sudo su hduser -c "ssh-keygen -t rsa -b 4096 -P \"\"; cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys" 
-sudo su hduser -c "ssh localhost; exit"
-# Generate SSH key for Hadoop
+##### Cleanup ########################
+rm hadoop-2.7.3.tar.gz rstudio-1.0.44-amd64.deb spark-2.0.1-bin-hadoop2.7.tgz Anaconda3-4.2.0-Linux-x86_64.sh zeppelin-0.6.2-bin-all.tgz
+cd $CWD
 
-# Set Hadoop env variables
-set_hadoop_envvars >> $HOME/.bashrc && source $HOME/.bashrc
-sudo su hduser -c "$(set_hadoop_envvars) >> ~/.bashrc; source ~/.bashrc"
+exit 0
 
-wget http://mirrors.ibiblio.org/apache/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
-tar xzvf hadoop-2.7.3.tar.gz
-sudo mv hadoop-2.7.3 /usr/local/share
-HADOOP_PATH=/usr/local/share/hadoop-2.7.3
-cd /usr/local/share && sudo ln -s $HADOOP_PATH hadoop && cd $HOME/Downloads
-
-sudo echo """
-<configuration>
-	<!-- file system properties -->
-	<property>
-		<name>fs.defaultFS</name>
-		<value>hdfs://localhost:50070/</value>
-	</property>
-</configuration>
-""" > $HADOOP_PATH/core-site.xml
-sudo echo """
-<configuration>
-	<property>
-		<name>dfs.replication</name>
-		<value>1</value>
-	</property>
-	<property>
-		<name>dfs.namenode.name.dir</name>
-		<value>${HADOOP_HOME}/hdfs/namenode</value>
-	</property>
-	<property>
-		<name>dfs.namenode.name.dir.restore</name>
-		<value>true</value>
-	</property>
-	<property>
-		<name>dfs.datanode.data.dir</name>
-		<value>${HADOOP_HOME}/hdfs/datanode</value>
-	</property>
-	<property>
-		<name>dfs.namenode.checkpoint.dir</name>
-		<value>${HADOOP_HOME}/hdfs/secondarynamenode</value>
-	</property>
-	<property>
-		<name>dfs.permissions</name>
-		<value>true</value>
-	</property>
-</configuration>
-""" > $HADOOP_PATH/hdfs-site.xml
-sudo echo """
-<configuration>
-	<property>
-		<name>mapreduce.cluster.temp.dir</name>
-		<value></value>
-		<final>true</final>
-	</property>
-	<property>
-		<name>mapreduce.cluster.local.dir</name>
-		<value></value>
-		<final>true</final>
-	</property>
-	<property>
-		<name>mapreduce.job.maps</name>
-		<value>2</value>
-	</property>
-	<property>
-		<name>mapreduce.job.reduces</name>
-		<value>1</value>
-	</property>
-	<property>
-		<name>mapreduce.jobtracker.taskscheduler</name>
-		<value>org.apache.hadoop.mapred.JobQueueTaskScheduler</value>
-	</property>
-	<property>
-		<name>mapreduce.job.counters.limit</name>
-		<value>120</value>
-	</property>
-</configuration>
-""" > $HADOOP_PATH/mapred-site.xml
-sudo echo """
-
-<configuration>
-	<property>
-		<name>mapreduce.framework.name</name>
-		<value>yarn</value>
-	</property>
-	<property>
-		<name>yarn.resourcemanager.scheduler.class</name>
-		<value>org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.CapacityScheduler</value>
-	</property>
-	<property>
-		<name>yarn.nodemanager.aux-services</name>
-		<value>mapreduce_shuffle</value>
-	</property>
-	<property>
-		<name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>
-		<value>org.apache.hadoop.mapred.ShuffleHandler</value>
-	</property>
-	<property>
-		<name>yarn.app.mapreduce.am.staging-dir</name>
-		<value>/tmp/hadoop-yarn/staging</value>
-	</property>
-</configuration>
-""" > $HADOOP_PATH/yarn-site.xml
-sudo perl -p -i -e 's/\{JAVA_HOME\}/\(readlink -f \/usr\/bin\/java | sed \"s:bin\/java::\"\)/g' /usr/local/share/hadoop/etc/hadoop-env.sh
-
-sudo chown -R hduser:hadoop $HADOOP_PATH
-
-sudo su hduser -c "hdfs namenode -format; start-dfs.sh; jps; start-yarn.sh"
-
-##### Install Spark #####
-SPARK_PATH=/usr/local/share/spark-2.0.1-bin-hadoop2.7
-set_spark_envvars >> $HOME/.bashrc && source $HOME/.bashrc
-sudo addgroup analysts
-sudo usermod -a -G analysts $(whoami)
-wget http://d3kbcqa49mib13.cloudfront.net/spark-2.0.1-bin-hadoop2.7.tgz
-tar xzvf spark-2.0.1-bin-hadoop2.7.tgz
-sudo mv spark-2.0.1-bin-hadoop2.7 /usr/local/share; cd /usr/local/share && sudo ln -s $SPARK_PATH spark && cd $HOME/Downloads; sudo chown -R $(whoami):analysts $SPARK_PATH
-sudo cp $SPARK_PATH/conf/spark-env.sh.template $SPARK_PATH/conf/spark-env.sh
-echo """
-JAVA_HOME=$(readlink -f /usr/bin/java | sed \"s:bin/java::\")
-SPARK_MASTER_IP=10.0.2.15
-SPARK_WORKER_MEMORY=3g
-""" >> $SPARK_PATH/conf/spark-env.sh
-
-##### Installing Anaconda #####
-wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh | bash
-
-##### Installing Apache Zeppelin
-sudo adduser --system --ingroup analysts zeppelin
-wget http://mirrors.ibiblio.org/apache/zeppelin/zeppelin-0.6.2/zeppelin-0.6.2-bin-all.tgz
-tar xzvf zeppelin-0.6.2-bin-all.tgz
-sudo mv zeppelin-0.6.2-bin-all /usr/local && sudo ln -s zeppelin-0.6.2-bin-all zeppelin
-sudo chown -R zeppelin:analysts /usr/local/share/zeppelin
-sudo echo """
-description \"zeppelin\"
-
-start on (local-filesystems and net-device-up IFACE!=lo)
-stop on shutdown
-
-# Respawn the process on unexpected termination
-respawn
-
-# respawn the job up to 7 times within a 5 second period.
-# If the job exceeds these values, it will be stopped and marked as failed.
-respawn limit 7 5
-
-# zeppelin was installed in /usr/share/zeppelin in this example
-chdir /usr/local/share/zeppelin
-exec bin/zeppelin-daemon.sh upstart
-""" > /etc/init/zeppelin.conf
-sudo service zeppelin start
+#fi
+#-------------------------------------
